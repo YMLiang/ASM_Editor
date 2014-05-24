02553bacf3a537b124025ccd62654359600f1a89
YMLiang <liangyaming006@gmail.com>
2014-05-24 16:26:34 +0800

    Syntax Highlighting succeed!
    Signed-off-by: YMLiang <liangyaming006@gmail.com>

--- src/edu/suda/ide/asm/editor/ASMCodeScanner.java
+++ src/edu/suda/ide/asm/editor/ASMCodeScanner.java
@@ -13,12 +13,15 @@ import org.eclipse.jface.text.rules.WhitespaceRule;
 import org.eclipse.jface.text.rules.WordRule;
 
 public class ASMCodeScanner extends RuleBasedScanner {
-	private static String[] fgkeywords = {"while","for","if","else"};
+	private static String[] fgkeywords = {"include","define","start_loop","delay_outer","delay_inner"};
+	private static String[] fginstructions = {"loadi","out","move","bne","addi","cpi"};
 	
 	public ASMCodeScanner(ASMColorProvider provider){
 		
 		IToken keyword = new Token(new 
 				TextAttribute(provider.getColor(ASMColorProvider.KEYWORD)));
+		IToken instruction = new Token(new 
+				TextAttribute(provider.getColor(ASMColorProvider.INSTRUCTION)));
 		IToken string = new Token(new 
 				TextAttribute(provider.getColor(ASMColorProvider.STRING)));
 		IToken comment = new Token(new 
@@ -38,11 +41,14 @@ public class ASMCodeScanner extends RuleBasedScanner {
 		// Add generic whitespace rule. 
 		rules.add(new WhitespaceRule(new MyWhitespaceDetector()));
 		
-		// Add word rule for keywords.
+		// Add word rule for keywords and instructions.
 		WordRule wordRule= new WordRule(new MyWordDetector(), other); 
 		for (int i= 0; i < fgkeywords.length; i++) 
 			wordRule.addWord(fgkeywords[i], keyword); 
+		for (int i= 0; i < fginstructions.length; i++) 
+			wordRule.addWord(fginstructions[i], instruction); 
 		rules.add(wordRule); 
+		
 		IRule[] result= new IRule[rules.size()]; 
 		rules.toArray(result); 
 		setRules(result);
--- src/edu/suda/ide/asm/editor/MyWordDetector.java
+++ src/edu/suda/ide/asm/editor/MyWordDetector.java
@@ -3,17 +3,12 @@ package edu.suda.ide.asm.editor;
 import org.eclipse.jface.text.rules.IWordDetector;
 
 public class MyWordDetector implements IWordDetector {
-    /**
-     * {@inheritDoc}
-     */
-    public boolean isWordStart(char c) {
-      return ((c > ' ') && (c <= '~'));
-    }
 
-    /**
-     * {@inheritDoc}
-     */
-    public boolean isWordPart(char c) {
-      return ((c > ' ') && (c <= '~'));
-    }
+	public boolean isWordPart(char character) {
+		return Character.isJavaIdentifierPart(character);
+	}
+
+	public boolean isWordStart(char character) {
+		return Character.isJavaIdentifierStart(character);
+	}
 }
